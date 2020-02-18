fde7f170ef111fe52b82902056e868d13bc5e27d
PatEvs <ppevans11@gmail.com>
2020-02-18 14:17:23 +1300

    ✨  Created 0_hello_world.js

---
+++ 0_hello_world.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import { AppRegistry, Text } from 'react-native';
+
+
+export default class HelloWorldApp extends Component {
+
+    render() {
+
+        return (
+
+        <Text>Hello world!</Text>
+
+        );
+
+    }
+
+}
+
+
+// Skip this line if using Create React Native App
+
+AppRegistry.registerComponent('HelloWorld', () => HelloWorldApp);
+
+
+// The React native code can also be imported from another component with the following code:
+
+
+import HelloWorldApp from './HelloWorldApp';
