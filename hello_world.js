f080b0b6839443a616f737044ab421e4d8f93ff6
PatEvs <ppevans11@gmail.com>
2020-02-18 14:54:07 +1300

    ✨  Formatting hello_world.js

--- hello_world.js
+++ hello_world.js
@@ -2,12 +2,22 @@
  *  hello_world.js
  */
 
+/*************
+ * * IMPORTS *
+ *************/
+
 import React, { Component } from 'react';
 import { AppRegistry, Text } from 'react-native';
 
+// The React native code can also be imported from another component with the following code:
+// import HelloWorldApp from './HelloWorldApp';
 
-export default class HelloWorldApp extends Component {
+/*********************
+ * * COMPONENT CLASS *
+ *********************/
 
+class HelloWorldApp extends Component {
+		// ..
     render() {
 
         return (
@@ -16,14 +26,20 @@ export default class HelloWorldApp extends Component {
 
         );
 
-    }
-
+		}
+		// ..
 }
 
 // Skip this line if using Create React Native App
 AppRegistry.registerComponent('HelloWorld', () => HelloWorldApp);
 
-// The React native code can also be imported from another component with the following code:
-// import HelloWorldApp from './HelloWorldApp';
+/*************
+ * * EXPORTS *
+ *************/
+
+// The following statement can be excluded if you declare the above class like:
+// 	export default class HelloWorldApp ...
+
+export default HelloWorldApp;
 
 /* EOF */
