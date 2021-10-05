2f3984d95e1f0b0a32907922fad6d3c87ec54d4e
Patrick Evans <ppevans11@gmail.com>
2021-10-05 13:07:42 +1300

    :recycle: Formatting

--- hello_world.js
+++ hello_world.js
@@ -1,10 +1,8 @@
 /**
- *  hello_world.js
+ * *	hello_world.js
  */
 
-/*************
- * * IMPORTS *
- *************/
+// * IMPORTS * //
 
 import React, { Component } from 'react';
 import { AppRegistry, Text } from 'react-native';
@@ -12,9 +10,7 @@ import { AppRegistry, Text } from 'react-native';
 // The React native code can also be imported from another component with the following code:
 // import HelloWorldApp from './HelloWorldApp';
 
-/*********************
- * * COMPONENT CLASS *
- *********************/
+// * APP COMPONENT * //
 
 class HelloWorldApp extends Component {
 		// ..
@@ -33,12 +29,10 @@ class HelloWorldApp extends Component {
 // Skip this line if using Create React Native App
 AppRegistry.registerComponent('HelloWorld', () => HelloWorldApp);
 
-/*************
- * * EXPORTS *
- *************/
+// * EXPORTS * //
 
 // The following statement can be excluded if you declare the above class like:
-// 	export default class HelloWorldApp ...
+// 	`export default class HelloWorldApp` ...
 
 export default HelloWorldApp;
 
